1a0536036d4eabac3bc45312c20231b16f0a3bb3
rohmerpauline <rohmer.pauline@outlook.com>
2024-02-27 18:43:57 +0100

    feat: create TaskStore

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import clickOutside from './directives/click-outside'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { createPinia } from 'pinia'
-library.add(faChevronDown)
+import {
+  faChevronDown,
+  faClock,
+  faCheck,
+  faExclamation,
+  faListCheck,
+  faPlus
+} from '@fortawesome/free-solid-svg-icons'
+library.add(faChevronDown, faListCheck, faClock, faCheck, faExclamation, faPlus)
 
 const app = createApp(App)
 const pinia = createPinia()
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -6,61 +6,88 @@ export const useTaskStore = defineStore('taskStore', {
       {
         id: 1,
         title: 'Clearly outline the scope and key requirements for development.',
-        deadline: '2024-02-27'
+        deadline: '2024-02-27',
+        isDone: false
       },
       {
         id: 2,
         title: 'Design a Home Page layout focusing on a visually appealing task summary.',
-        deadline: '2024-02-27'
+        deadline: '2024-02-27',
+        isDone: false
       },
       {
         id: 3,
         title: 'Develop a reusable component for adding tasks with priority settings.',
-        deadline: '2024-02-27'
+        deadline: '2024-02-27',
+        isDone: false
       },
       {
         id: 4,
         title: 'Update TaskItem component to visually represent task priorities.',
-        deadline: '2024-02-27'
+        deadline: '2024-02-27',
+        isDone: false
       },
-      { id: 5, title: 'Set up Vuex for global state management of tasks.', deadline: '2024-02-27' },
+      {
+        id: 5,
+        title: 'Set up Vuex for global state management of tasks.',
+        deadline: '2024-02-27',
+        isDone: false
+      },
       {
         id: 6,
         title: 'Add filtering options for tasks on the ToDoList page.',
-        deadline: '2024-03-15'
+        deadline: '2024-03-15',
+        isDone: false
       },
       {
         id: 7,
         title: 'Design CompletedTasks page layout for displaying completed tasks.',
-        deadline: '2024-03-15'
+        deadline: '2024-03-15',
+        isDone: false
       },
-      { id: 8, title: 'Refine CSS styles for a responsive design.', deadline: '2024-03-15' },
+      {
+        id: 8,
+        title: 'Refine CSS styles for a responsive design.',
+        deadline: '2024-03-15',
+        isDone: false
+      },
       {
         id: 9,
         title: 'Experiment with Vue transition for subtle animations.',
-        deadline: '2024-02-26'
+        deadline: '2024-02-26',
+        isDone: false
       },
       {
         id: 10,
         title: 'Integrate local storage for persistent task data.',
-        deadline: '2024-06-10'
+        deadline: '2024-06-10',
+        isDone: false
       },
       {
         id: 11,
         title: 'Create comprehensive Readme documentation for the project.',
-        deadline: '2024-05-12'
+        deadline: '2024-05-12',
+        isDone: false
       },
       {
         id: 12,
         title: 'Conduct thorough testing of undo functionality in CompletedTasks.',
-        deadline: '2024-03-01'
+        deadline: '2024-03-01',
+        isDone: false
       }
     ],
     selectedFilter: ''
   }),
   getters: {
+    dueToday(state) {
+      return state.tasks.filter(
+        (task) => new Date(task.deadline).toDateString() === new Date().toDateString()
+      )
+    },
     outdated(state) {
-      return state.tasks.filter((task) => new Date(task.deadline) < new Date())
+      const startOfToday = new Date()
+      startOfToday.setHours(0, 0, 0, 0)
+      return state.tasks.filter((task) => new Date(task.deadline).getTime() < startOfToday)
     }
   }
 })
